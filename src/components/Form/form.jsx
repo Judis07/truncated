c8a6b8eeab0440d66416860bec7c6e0c025e8578
Arvind M <arvind@kraftshala.com>
2020-12-01 19:45:13 +0530

    react state added to form component

--- src/components/Form/form.jsx
+++ src/components/Form/form.jsx
@@ -1,18 +1,44 @@
+import { useState } from 'react';
+import Loader from './Loader/loader';
+
 const Form = () => {
+  const [urlValue, setUrlValue] = useState('');
+  const [loading, setLoading] = useState(false);
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setUrlValue(value);
+  };
+
+  const submit = (event) => {
+    event.preventDefault();
+    setLoading(true);
+
+    console.log(urlValue);
+    setUrlValue('');
+  };
+
   return (
     <div className="form-container">
       <div className="container">
-        <form className="form-div">
+        <form onSubmit={submit} className="form-div">
           <div className="input-container">
             <input
               type="url"
+              value={urlValue}
+              onChange={handleChange}
               className="link-input"
               placeholder="Paste your link here.."
               required
             />
           </div>
           <div className="cta">
-            <button className="truncate-btn">Truncate Me!</button>
+            {loading ? (
+              <button className="truncate-btn not-allowed">
+                <Loader />
+              </button>
+            ) : (
+              <button className="truncate-btn pointer">Truncate Me!</button>
+            )}
           </div>
         </form>
       </div>
